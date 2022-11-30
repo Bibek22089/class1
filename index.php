dd34a304d8b371d9ef9df1868d4eb057d0b0dfe5
Bibek22089 <108653184+Bibek22089@users.noreply.github.com>
2022-11-30 08:51:24 +0545

    Add files via upload

---
+++ index.php
@@ -0,0 +1,29 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>i am new one </title>
+    <h1>i am a hacker</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima distinctio exercitationem consequatur porro harum, saepe aliquid excepturi dignissimos architecto ullam error dolorem ex 
+        impedit, placeat beatae nemo rem voluptates nisi!</p>
+</head>
+<body>
+<?php
+$a=5;
+$b=6 ;
+echo $a+$b,"<br>" ;
+$txt= "FOOTBALL";
+ECHO "HELLO WORLLD! <BR>";
+echo "i love $txt,<BR>";
+echo "i am bibek bhattarai<br>";
+echo "Hello wordld <br>";
+$x=7;
+$r=9;
+echo $x+$r,"<br>";
+echo"hello";
+?>
+</body>
+</html>
